8bebaea5ec4ce99cd22faaa71d9e7907590d4fb7
vlad olenin <volenin@gmail.com>
2019-11-13 14:04:45 -0500

    test

--- src/main/java/com/acme/acmetrade/endpoints/TraderController.java
+++ src/main/java/com/acme/acmetrade/endpoints/TraderController.java
@@ -1,5 +1,9 @@
 package com.acme.acmetrade.endpoints;
 
+import com.acme.acmetrade.domain.Trader;
+import com.acme.acmetrade.services.TradersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("trader")
 public class TraderController {
 
+    @Autowired
+    private final TradersService tradersService;
+
     @GetMapping(path = "Traders", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> getTrader(){
-        return  new ResponseEntity<>(new Trader("Account"))
+        return  new ResponseEntity<>(tradersService.getAllTraders(), HttpStatus.OK);
     }
 
 }
